92b5bfecec33580ecc4adb82596732127a2d67e8
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-04-03 22:18:57 +0700

    Kemudian rubah pada file main.dart dengan memanggil class HomeView

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:data_kontak/screen/hal_register.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,14 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Data Kontak",
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Data Kontak"),
-            centerTitle: true,
-          ),
-          body: FormKontak()),
+      title: "Data Kontak",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+        ),
+        useMaterial3: true,
+      ),
+      home: HomeView(),
     );
   }
 }
