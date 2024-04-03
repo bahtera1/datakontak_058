1cf00ce900fe530da7d15d8d2576401acfa75de8
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2024-04-03 21:58:35 +0700

    tambah fungsi untuk mengambil data dari API di kontakservice

--- lib/controller/kontak_controller.dart
+++ lib/controller/kontak_controller.dart
@@ -43,4 +43,16 @@ class KontakController {
       return {"success": false, "message": 'Terjadi kesalahan: $e'};
     }
   }
+
+  Future<List<Person>> getPeople() async {
+    try {
+      List<dynamic> peopleData = await kontakService.fetchPeople();
+      List<Person> people =
+          peopleData.map((json) => Person.fromMap(json)).toList();
+      return people;
+    } catch (e) {
+      print(e);
+    }
+    throw Exception('Failed to get people');
+  }
 }
